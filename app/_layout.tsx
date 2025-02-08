e4834afb1984ea8e2f65b05774f3926d5464f360
Emmanuel Egomson <fegomson@gmail.com>
2025-02-08 10:15:18 -0600

    Creating the basic layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
+import { COLORS } from "@/constants/Colors";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+    <StatusBar barStyle="light-content" />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { 
+          backgroundColor: COLORS.white
+        },
+        animation: "slide_from_right",
+        header: () => null,
+        navigationBarHidden: true,
+      }}
+    >
+      <Stack.Screen name="index" options={{
+        headerShown: false,
+      }} />
+    </Stack>
+  </>
+  )
 }
